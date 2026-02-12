f989d9a20ef5d5decabb590b9f8adbd017177c76
K33run <Kirandc466@gmail.com>
2026-02-12 01:56:38 -0500

    duim.py

--- duim.py
+++ duim.py
@@ -10,7 +10,7 @@ OPS445 Assignment 2 - Winter 2022
 Program: duim.py 
 Author: "Kiran Dangi"
 The python code in this file (duim.py) is original work written by
-"Bijay Sharma". No code in this file is copied from any other source 
+"Kiran Dangi". No code in this file is copied from any other source 
 except those provided by the course instructor, including any person, 
 textbook, or on-line resource. I have not shared this python script 
 with anyone or anything except for submission for grading.  
